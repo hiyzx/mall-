946496a335cbe0cb5f46e3ef6bbcadc769611265
hiyzx <hiyzx>
2019-12-23 20:15:33 +0800

    添加consul配置

--- api/bi-pms/src/main/java/org/zero/mall/pms/config/WebConfigurer.java
+++ api/bi-pms/src/main/java/org/zero/mall/pms/config/WebConfigurer.java
@@ -2,16 +2,11 @@ package org.zero.mall.pms.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/**
- * @author yezhaoxing
- * @date 2017/7/17
- */
 @Configuration
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
+public class WebConfigurer implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -21,12 +16,8 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     }
 
     @Override
-    public void configurePathMatch(PathMatchConfigurer configurer) {
-        configurer.setUseSuffixPatternMatch(false);
-    }
-
-    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
     }
+
 }
